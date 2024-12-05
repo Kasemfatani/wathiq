cfbb340942ffce4dbd43666dd6ed765c8e7a4b16
Ammar <ammar@i-esnaad.com>
2024-12-05 15:01:01 +0300

    add extra customers

--- src/components/home/Parteners.jsx
+++ src/components/home/Parteners.jsx
@@ -9,15 +9,18 @@ import part4 from '/public/customers/4.png';
 import part5 from '/public/customers/5.png';
 import part6 from '/public/customers/6.png';
 import part7 from '/public/customers/7.png';
+import part8 from '/public/customers/8.png';
+import part9 from '/public/customers/9.png';
+import part10 from '/public/customers/10.png';
 import NumberTicker from '../ui/number-ticker';
 
 export default function Parteners() {
-    let parts = [part1, part2, part3, part4, part5, part6, part7];
+    let parts = [part1, part2, part3, part4, part5, part6, part7,part8,part9,part10];
     let numbers = [
-        { name: "وفرنا", number: 500000, p: "ريال سعودي لكل عميل في منتج واحد" },
-        { name: "أنجزنا", number: 12000, p: "رحله عبر النظام" },
-        { name: "نقلنا", number: 430, p: "مليون لتر" },
-        { name: "استخدمنا", number: 40000, p: "قفلا مخصصا" },
+        { name: "وفرنا", number: 500000, p: "ريال سعودي لعميل واحد في منتج واحد" },
+        { name: "أنجزنا", number: 17000, p: "رحله عبر النظام" },
+        { name: "نقلنا", number: 580, p: "مليون لتر" },
+        { name: "استخدمنا", number: 60000, p: "قفلا مخصصا" },
     ]
     return (
         <div className="parteners" >

39b5bb57947973323f6779ca673d0a3a10af6c55
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-04-24 11:31:44 +0200

    Update page.jsx

--- src/app/congats/page.jsx
+++ src/app/congats/page.jsx
@@ -1,45 +1,45 @@
-'use client'
+'use client';
 import React, { Suspense, useEffect } from 'react';
 import doneImage from '/public/done.svg';
 import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
 import Loading from '../loading';
 
-export default function page() {
+export default function Page() {
+  return (
     <Suspense fallback={<Loading />}>
-        <Congats />
+      <Congats />
     </Suspense>
+  );
 }
 
 function Congats() {
-    const searchParams = useSearchParams();  // Fixed variable name
-    const name = searchParams.get('name');
-    const phone = searchParams.get('phone');
-    const emil = searchParams.get('email');
-    const size = searchParams.get('size');
+  const searchParams = useSearchParams();
+  const name = searchParams.get('name');
+  const phone = searchParams.get('phone');
+  const email = searchParams.get('email'); // fixed typo from "emil"
+  const size = searchParams.get('size');
 
-    useEffect(() => {
-        // Ensure window object is available before accessing dataLayer
-        if (typeof window !== "undefined") {
-            window.dataLayer = window.dataLayer || [];
-            window.dataLayer.push({
-                event: 'form_submission',
-                customer_name: name,
-                customer_phone: phone,
-                customer_organization_size: size,
-                customer_email: emil
-            });
-        }
-    }, [name, phone, size, emil]); // Added dependency array
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: 'form_submission',
+        customer_name: name,
+        customer_phone: phone,
+        customer_organization_size: size,
+        customer_email: email,
+      });
+    }
+  }, [name, phone, size, email]);
 
-
-    return (
-        <div className="popup" >
-            <div className="popup-cont">
-                <Image src={doneImage} alt="Wathiq" className="img" />
-                <h2>مرحبا {name} </h2>
-                <p>تم استلام طلبك بنجاح ، سيتم التواصل معك قريبا</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="popup">
+      <div className="popup-cont">
+        <Image src={doneImage} alt="Wathiq" className="img" />
+        <h2>مرحبا {name}</h2>
+        <p>تم استلام طلبك بنجاح ، سيتم التواصل معك قريبا</p>
+      </div>
+    </div>
+  );
 }
